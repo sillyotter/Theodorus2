d41f421a86cda338ec3910aa1c2efc227f08abdf
Guy Oliver <guy.oliver@gmail.com>
2013-07-25 18:43:54 -0400

    Added stub xUnit unit test and setup project to build installation media

---
+++ UnitTests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            await Task.Delay(100);
+            Assert.True(true);
+        }
+    }
+}
